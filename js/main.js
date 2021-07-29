a72623a268535804d6e4366a4e0a53b922bb747b
waroach <brent.rankin@gmail.com>
2021-07-28 17:08:31 -0700

    Saved Changes to War

--- js/main.js
+++ js/main.js
@@ -1,46 +1,45 @@
 let suits = ['Spades','Diamonds','Clubs','Hearts']
 let values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+let deck=[],hand1=[],hand2=[]
+
+document.querySelector('#play').addEventListener(onclick, makeDecks())
 
 function makeDecks(){
-    let deck = new Array()
-
     for(i=0;i<suits.length;i++){
         for(x=0;x<values.length;x++){
             let card = {Value: values[x], Suit: suits[i]}
             deck.push(card)
         }
     }
-    //console.log(deck)
-    let hand1 = deck.splice(26)
-    let hand2 = deck.splice(0,26)
-    console.log(hand1)
-    console.log(hand2)
+    hand1 = deck.splice(26)
+    hand2 = deck.splice(0,26)
 }
 
 
 function renderDeck(deck){
-    document.getElementById("deck").innerHTML = "";
+    document.getElementById("deck").innerHTML = ""
 
 	for(let i = 0; i < deck.length; i++)
 	{
-		let card = document.createElement("div");
-		let value = document.createElement("div");
-		let suit = document.createElement("div");
-		card.className = "card";
-		value.className = "value";
-		suit.className = "suit " + deck[i].Suit;
+		let card = document.createElement("div")
+		let value = document.createElement("div")
+		let suit = document.createElement("div")
+		card.className = "card"
+		value.className = "value"
+		suit.className = `suit ${deck[i].Suit}`
 
-		value.innerHTML = deck[i].Value;
-		card.appendChild(value);
-		card.appendChild(suit);
+		value.innerHTML = deck[i].Value
+		card.appendChild(value)
+		card.appendChild(suit)
 
-		document.getElementById("deck").appendChild(card);
+		document.getElementById("deck").appendChild(card)
 	}
 }
 
 
 makeDecks()
-
+console.log(hand1)
+console.log(hand2)
 
 
 
